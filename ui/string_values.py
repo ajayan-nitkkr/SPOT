ff70f01ac3c4a7387ad22f43ec22d038e08b9d0a
Ajay Anand <anandajay1834@gmail.com>
2018-03-11 10:13:38 -0700

    adding the constant files missed in last commit

--- ui/string_values.py
+++ ui/string_values.py
@@ -4,7 +4,18 @@ LABEL_VIDEO_PATH = 'Video path:'
 LABEL_PROCESS = 'Process'
 LABEL_VIDEO_HOT = 'Is this video white hot?'
 LABEL_VIDEO_BORDER = 'Are there any numbers or borders in the video?'
+LABEL_LOCAL_SERVER = 'Local'
+LABEL_REMOTE_SERVER = 'Remote'
+LABEL_YES = 'Yes'
+LABEL_NO = 'No'
 
 
 COLOR_GREEN = 'green'
 COLOR_GRAY = 'gray'
+
+DICT_CROP_VIDEOHOT_KEY = 'invert'
+DICT_CROP_VIDEOBORDER_KEY = 'crop'
+DICT_CROP_CROPTOP_KEY = 'cropTop'
+DICT_CROP_CROPLEFT_KEY = 'cropLeft'
+DICT_CROP_CROPBOTTOM_KEY = 'cropBottom'
+DICT_CROP_CROPRIGHT_KEY = 'cropRight'
